d9924beb9fedc0c1cebff8a934b970be77133175
Juan Ignacio Avelar Miranda <38658482+juanignacioavelar@users.noreply.github.com>
2018-11-07 11:48:29 -0600

    Update Practica_06.asm

--- Practica_06/Practica_06.asm
+++ Practica_06/Practica_06.asm
@@ -1,36 +1,36 @@
-;-------------- Archivos de inclusi髇 (Librer韆s) -----------------------------
-#include "p16f84a.inc"			; Librer韆 del PIC16F84A
-;-------------- Bits de Configuraci髇 -----------------------------------------
+;-------------- Archivos de inclusi贸n (Librer铆as) -----------------------------
+#include "p16f84.inc"				; Librer铆a del PIC16F84
+;-------------- Bits de Configuraci贸n -----------------------------------------
  __CONFIG _XT_OSC & _WDT_OFF & _PWRTE_ON & _CP_OFF
 ;-------------- Vector de reset -----------------------------------------------  
-				ORG		0x000			; Vector de reset del procesador
-				GOTO	CONFIGURACION	; Nos manda a la etiqueta CONFIGURACION
-				ORG		0x005
+		ORG	0x000			; Vector de reset del procesador
+		GOTO	CONFIGURACION		; Nos manda a la etiqueta CONFIGURACION
+		ORG	0x005
 ;============== Programa Principal ============================================
-;-------------- Configuraci髇 del microcontrolador ----------------------------
+;-------------- Configuraci贸n del microcontrolador ----------------------------
 CONFIGURACION	CLRF    PORTA			; Limpia el registro PORTA
-				CLRF    PORTB			; Limpia el registro PORTB
-				BSF		STATUS,RP0		; Nos movemos al Banco 1
-				BSF     TRISA,RA0		; Configuramos el pin RA0 como entrada
-				BSF     TRISA,RA2		; Configuramos el pin RA2 como entrada
-				BCF		TRISB,RB0		; Configuramos el pin RB0 como salida
-				BCF		STATUS,RP0		; Regresamos al Banco 0
-;-------------- Aplicaci髇 control de NIVEL -----------------------------------
-COMP1			BTFSS	PORTA,RA0		; Compara si RA0=1 y brinca a comparar RA2
-				GOTO	COMP2			; si no, brinca a la etiqueta COMP2
-				BTFSS	PORTA,RA2		; Compara si RA2=1 y brinca a apagar la bomba
-				GOTO	COMP2			; si no, brinca a la etiqueta COMP2
-				GOTO	APAGAR			; Brinca a la subrutina APAGAR
-COMP2			BTFSC	PORTA,RA0		; Compara si RA0=0 y brinca a comparar RA2
-				GOTO	COMP1			; si no, brinca a la etiqueta COMP1
-				BTFSC	PORTA,RA2		; Compara si RA2=0 y brinca a encender la bomba
-				GOTO	COMP1			; si no, brinca a la etiqueta COMP1
-				GOTO	ENCENDER		; Brinca a la subrutina ENCENDER
+		CLRF    PORTB			; Limpia el registro PORTB
+		BSF	STATUS,RP0		; Nos movemos al Banco 1
+		BSF     TRISA,RA0		; Configuramos el pin RA0 como entrada
+		BSF     TRISA,RA2		; Configuramos el pin RA2 como entrada
+		BCF	TRISB,RB0		; Configuramos el pin RB0 como salida
+		BCF	STATUS,RP0		; Regresamos al Banco 0
+;-------------- Aplicaci贸n control de NIVEL -----------------------------------
+COMP1		BTFSS	PORTA,RA0		; Compara si RA0=1 y brinca a comparar RA2
+		GOTO	COMP2			; si no, brinca a la etiqueta COMP2
+		BTFSS	PORTA,RA2		; Compara si RA2=1 y brinca a apagar la bomba
+		GOTO	COMP2			; si no, brinca a la etiqueta COMP2
+		GOTO	APAGAR			; Brinca a la subrutina APAGAR
+COMP2		BTFSC	PORTA,RA0		; Compara si RA0=0 y brinca a comparar RA2
+		GOTO	COMP1			; si no, brinca a la etiqueta COMP1
+		BTFSC	PORTA,RA2		; Compara si RA2=0 y brinca a encender la bomba
+		GOTO	COMP1			; si no, brinca a la etiqueta COMP1
+		GOTO	ENCENDER		; Brinca a la subrutina ENCENDER
 ;-------------- Subrutina prender bomba de agua -------------------------------
-ENCENDER		BSF		PORTB,RB0		; Manda un 1 en RB0 para encender el led
-				GOTO	COMP2			; Brinca a la etiqueta COMP2
+ENCENDER	BSF	PORTB,RB0		; Manda un 1 en RB0 para encender el led
+		GOTO	COMP2			; Brinca a la etiqueta COMP2
 ;-------------- Subrutina prender bomba de agua -------------------------------
-APAGAR			BCF		PORTB,RB0		; Manda un 0 en RB0 para apagar el led
-				GOTO	COMP1			; Brinca a la etiqueta COMP1
+APAGAR		BCF	PORTB,RB0		; Manda un 0 en RB0 para apagar el led
+		GOTO	COMP1			; Brinca a la etiqueta COMP1
 ;============== Fin del Programa ==============================================
-				END						; Le indica al ensamblador el final
+		END						; Le indica al ensamblador el final
