4dae2e11bf45830f82307fe8d3715d1dfb61ffd1
Juan Ignacio Avelar Miranda <38658482+juanignacioavelar@users.noreply.github.com>
2018-11-07 11:31:58 -0600

    Update Practica_02.asm

--- Practica_02/Practica_02.asm
+++ Practica_02/Practica_02.asm
@@ -1,28 +1,28 @@
-;-------------- Archivos de inclusi髇 (Librer韆s) -----------------------------
-#include "p16f84a.inc"				  ; Librer韆 del PIC16F84A
-;-------------- Bits de Configuraci髇 -----------------------------------------
+;-------------- Archivos de inclusi贸n (Librer铆as) -----------------------------
+#include "p16f84a.inc"			; Librer铆a del PIC16F84A
+;-------------- Bits de Configuraci贸n -----------------------------------------
  __CONFIG _FOSC_XT & _WDTE_OFF & _PWRTE_OFF & _CP_OFF
 ;-------------- Vector de reset -----------------------------------------------
-				ORG		0x000		  ; Vector de reset del procesador
-				GOTO	CONFIGURACION ; Nos manda a la etiqueta CONFIGURACION
-				ORG		0x005
+		ORG	0x000		; Vector de reset del procesador
+		GOTO	CONFIGURACION 	; Nos manda a la etiqueta CONFIGURACION
+		ORG	0x005
 ;============== Programa Principal ============================================
-;-------------- Configuraci髇 del microcontrolador ----------------------------
-CONFIGURACION	CLRF    PORTA		  ; Limpia el registro PORTA
-				CLRF    PORTB		  ; Limpia el registro PORTB
-				BSF		STATUS,RP0	  ; Nos movemos al Banco 1
-				BSF		TRISA,RA0	  ; Configuramos el pin RA0 como entrada	
-				BCF		TRISB,RB0	  ; Configuramos el pin RB0 como salida
-				BCF		STATUS,RP0	  ; Regresamos al Banco 0
-;-------------- Aplicaci髇 encender LEDs --------------------------------------
-LED				BTFSC	PORTA,RA0	  ; Si es cero se brinca a la siguiente instr
-				GOTO	LED_ON		  ; Nos manda a la etiqueta LED_ON
-				GOTO	LED_OFF		  ; Nos manda a la etiqueta LED_OFF
+;-------------- Configuraci贸n del microcontrolador ----------------------------
+CONFIGURACION	CLRF    PORTA		; Limpia el registro PORTA
+		CLRF    PORTB		; Limpia el registro PORTB
+		BSF	STATUS,RP0	; Nos movemos al Banco 1
+		BSF	TRISA,RA0	; Configuramos el pin RA0 como entrada	
+		BCF	TRISB,RB0	; Configuramos el pin RB0 como salida
+		BCF	STATUS,RP0	; Regresamos al Banco 0
+;-------------- Aplicaci贸n encender LEDs --------------------------------------
+LED		BTFSC	PORTA,RA0	; Si es cero se brinca a la siguiente instr
+		GOTO	LED_ON		; Nos manda a la etiqueta LED_ON
+		GOTO	LED_OFF		; Nos manda a la etiqueta LED_OFF
 ;-------------- Subrutina LED_ON ----------------------------------------------
-LED_ON			BSF		PORTB,RB0	  ; Enciende el LED en RB0
-				GOTO	LED			  ; Nos manda a la etiqueta LED
+LED_ON		BSF	PORTB,RB0	; Enciende el LED en RB0
+		GOTO	LED		; Nos manda a la etiqueta LED
 ;-------------- Subrutina LED_OFF ---------------------------------------------
-LED_OFF			BCF		PORTB,RB0	  ; Apaga el LED en RB0
-				GOTO	LED			  ; Nos manda a la etiqueta LED
+LED_OFF		BCF	PORTB,RB0	; Apaga el LED en RB0
+		GOTO	LED		; Nos manda a la etiqueta LED
 ;============== Fin del Programa ==============================================
-				END					  ; Le indica al ensamblador el final
+		END			; Le indica al ensamblador el final
