c93281244621809ff8cfc3721d689f2fe149564b
Juan Ignacio Avelar Miranda <38658482+juanignacioavelar@users.noreply.github.com>
2022-09-12 22:07:00 -0500

    Update Practica_01.asm

--- Practica_01/Practica_01.asm
+++ Practica_01/Practica_01.asm
@@ -1,23 +1,27 @@
+;-----------------------------------------------------------------------
+; Práctica 1: Lenguaje Ensamblador 					|
+; Microcontrolador: PIC16F84A Frecuencia: 4MHz Voltaje:5V 		|
+; Herramienta: MPLAB v8.92 – Microchip MPASM Toolsuite 			|
+;-----------------------------------------------------------------------
 ;-------------- Microcontrolador a usar ---------------------------------------
-		LIST	P=16F84		; Usaremos el PIC16F84
-		RADIX	HEX		; Valores hexadecimales
+		LIST	P=16F84A	; Usaremos el PIC16F84A
+		RADIX	HEX		;
 ;-------------- Variables a usar ----------------------------------------------
-STATUS		EQU	0x03		; Declaramos la variable STATUS
-PORTB		EQU	0x06		; Declaramos la variable PORTB
-TRISB		EQU	0x86		; Declaramos la variable TRISB
-RP0		EQU	0x05		; Declaramos la variable RP0
+STATUS		EQU	H'003'		; Declaramos la variable STATUS
+PORTB		EQU	H'006'		; Declaramos la variable PORTB
+TRISB		EQU	H'086'		; Declaramos la variable TRISB
+RP0		EQU	H'005'		; Declaramos la variable RP0
 ;-------------- Vector de reset -----------------------------------------------
-		ORG	0x000		; Vector de reset del procesador
+		ORG	H'000'		; Vector de reset del procesador
 		GOTO	CONFIGURACION	; Nos manda a la etiqueta CONFIGURACION
-		ORG	0x005
+		ORG	H'005'		; Se escribe nuestro código en 0x005
 ;============== Programa Principal ============================================
 ;-------------- Configuración del microcontrolador ----------------------------
 CONFIGURACION	BSF	STATUS,RP0	; Nos movemos al Banco 1
 		CLRF	TRISB		; Configura todo el puerto B como salida
 		BCF	STATUS,RP0	; Nos movemos al Banco 0
 ;-------------- Aplicación encender LEDs --------------------------------------
-LEDS		MOVLW	b'00000001'	; Guardamos un valor en el registro W
+LEDS		MOVLW	B'00000001'	; Guardamos un valor en el registro W
 		MOVWF	PORTB		; Movemos del registro W al puerto B
-		SLEEP			; Manda a dormir el CPU
 ;============== Fin del Programa ==============================================
 		END			; Le indica al ensamblador el final
